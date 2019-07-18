c9a66edc101047fe259afbbf60b2509886383897
zhweixi@sina.cn <zhweixi@sina.cn>
2019-07-18 16:42:33 +0800

    no message

--- server/resources/views/doctor/pets.blade.php
+++ server/resources/views/doctor/pets.blade.php
@@ -55,6 +55,80 @@
             background-color:#f5fafe;
         }
 
+
+        #pull_right{
+            text-align:center;
+        }
+        .pull-right {
+            /*float: left!important;*/
+        }
+        .pagination {
+            display: inline-block;
+            padding-left: 0;
+            margin: 20px 0;
+            border-radius: 4px;
+        }
+        .pagination > li {
+            display: inline;
+        }
+        .pagination > li > a,
+        .pagination > li > span {
+            position: relative;
+            float: left;
+            padding: 6px 12px;
+            margin-left: -1px;
+            line-height: 1.42857143;
+            color: #428bca;
+            text-decoration: none;
+            background-color: #fff;
+            border: 1px solid #ddd;
+        }
+        .pagination > li:first-child > a,
+        .pagination > li:first-child > span {
+            margin-left: 0;
+            border-top-left-radius: 4px;
+            border-bottom-left-radius: 4px;
+        }
+        .pagination > li:last-child > a,
+        .pagination > li:last-child > span {
+            border-top-right-radius: 4px;
+            border-bottom-right-radius: 4px;
+        }
+        .pagination > li > a:hover,
+        .pagination > li > span:hover,
+        .pagination > li > a:focus,
+        .pagination > li > span:focus {
+            color: #2a6496;
+            background-color: #eee;
+            border-color: #ddd;
+        }
+        .pagination > .active > a,
+        .pagination > .active > span,
+        .pagination > .active > a:hover,
+        .pagination > .active > span:hover,
+        .pagination > .active > a:focus,
+        .pagination > .active > span:focus {
+            z-index: 2;
+            color: #fff;
+            cursor: default;
+            background-color: #428bca;
+            border-color: #428bca;
+        }
+        .pagination > .disabled > span,
+        .pagination > .disabled > span:hover,
+        .pagination > .disabled > span:focus,
+        .pagination > .disabled > a,
+        .pagination > .disabled > a:hover,
+        .pagination > .disabled > a:focus {
+            color: #777;
+            cursor: not-allowed;
+            background-color: #fff;
+            border-color: #ddd;
+        }
+        .clear{
+            clear: both;
+        }
+
     </style>
     <script src="https://cdn.bootcss.com/jquery/2.1.1/jquery.min.js"></script>
     <script type="text/javascript">
@@ -96,9 +170,9 @@
     <th>诊断记录</th>
     <th >操作</th>
     </thead>
-    @foreach($pets as $pet)
+    @foreach($pets as $key => $pet)
     <tr class="a1">
-        <td>1</td>
+        <td>{{$key+1}}</td>
         <td>{{$pet->pid}}</td>
         <td><div style="padding:5px;">{!! $pet->content !!}</div></td>
         <td>{{$pet->created_at}}</td>
@@ -107,6 +181,14 @@
     </tr>
     @endforeach
 </table>
+
+
+
+<div id="pull_right">
+    <div class="pull-right">
+        {!! $pets->appends(['keywords' => $keywords])->render() !!}
+    </div>
+</div>
 
 </body>
 </html>
